71c2607198b54cd8b29f9411736b9d99c4de3351
Lingchang Meng <103092842+Piccabo0@users.noreply.github.com>
2022-04-28 22:35:15 +0800

    Add files via upload

--- Controll/main.c
+++ Controll/main.c
@@ -18,12 +18,13 @@ static void MX_GPIO_Init(void);
 static void MX_UART4_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_USART3_UART_Init(void);
-void Light_Task(void);//四路灯控制
+void Light_Task(void);//四路灯清空显示
 void Mode_Config(void);
 void Infrared_Avoid(void);
 void button_detect(void);
-void Hall_detect(void);
-void speed_Config(uint8_t* speed);
+void Hall_run(void);
+void speed_Config(uint16_t left, uint16_t right);
+void delay_ms(unsigned int ms);
 
 //RFID&磁导航共同控制量
 int CARD;
@@ -34,9 +35,10 @@ int COUNT=0;
 
 
 //按键
-int BUTTON_RED;
-int BUTTON_GREEN;
-int BUTTON_BLUE;
+int BUTTON_RED=1;
+int BUTTON_GREEN=1;
+int BUTTON_BLUE=1;\
+int STOP=0;//红外避障FLAG
 
 //控制数据帧定义
 //将uint8_t别名为无符号字符型
@@ -50,52 +52,52 @@ int main(void)
 {
   HAL_Init();//HAL库初始化
   SystemClock_Config();//初始化系统时钟
-
   MX_GPIO_Init();//GPIO引脚初始化
   MX_UART4_Init();//磁导航传感器串口初始化
   MX_USART1_UART_Init();//底盘通讯串口
   MX_USART3_UART_Init();//RFID读卡器串口初始化
-
+	
+	Light_Task();//清除灯的输出
+	
   printf("---- CAU Sensors Demo ----\r\n");
   printf("Build Time: %s, %s\r\n", __DATE__, __TIME__);
 	Mode_Config();//配置小车的运行模式==速度模式
 	
+	
+	
   while (1)
   {
-		button_detect();
-		//-------------------按下绿色按键启动，绿灯亮黄灯灭；否则，绿灯灭黄灯亮----------
+		button_detect();	
+		//-------------------按下绿色按键启动----------
 		if( BUTTON_GREEN==0)
 		{
-			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);//黄灯灭
-			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_SET);//绿灯亮
-			UART_Task();                    
-			MODBUS_READ_HALL_SERSOR_Task(); 
-			READ_RFID_BLOCK_Task();
-			Hall_detect();
-			Infrared_Avoid();//红外避障
-			
+			Infrared_Avoid();//红外避障			
+			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_RESET);//红灯灭		
+			if(STOP==0)
+			{
+				HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_SET);//绿灯亮
+				UART_Task();                    
+				MODBUS_READ_HALL_SERSOR_Task(); 
+				READ_RFID_BLOCK_Task();
+				Hall_run();			
+			}
+			else
+			{
+				HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_RESET);//绿灯灭
+				HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_SET);//红灯亮@
+				FillUartTxBufN((uint8_t*)stop, 16, 1);
+			}
 		}
 		else
 		{
 			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_RESET);//绿灯灭
 			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);//蓝灯灭
-			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);//黄灯亮
-			//HAL_Delay(10);
-			speed[7] = 0x00;
-			speed[9] = 0x00;
-			speed[11] = 0x00;
-			speed[8] = 0x00;
-			speed[13] = 0x00;
-			speed[12] = 0x00;
-			cal_crc=ModBus_CRC16_Calculate(speed , sizeof(speed)-2);
-			speed[14]=cal_crc&0xFF;
-			speed[15]=cal_crc>>8;  
-			FillUartTxBufN((uint8_t*)speed, 16, 1);
+			HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_SET);//红灯亮
+			FillUartTxBufN((uint8_t*)stop, 16, 1);
 		 }
 		
 		//------------------------------------------------------------------------
 		
-		//Light_Task();
     if(debug_show)
     {
       debug_show=0;
@@ -110,29 +112,22 @@ int main(void)
          FillUartTxBufN((uint8_t*)test_buffer, strlen(test_buffer), DEBUG_USART_ENUM);        
       }		
     }		
-  }
+  }//while
+}//main
 
+void delay_ms(unsigned int ms)
+{
+     SysTick_Config(72000);
+     for(int i = 0 ; i < ms ;i++)  
+    {
+      	while(!((SysTick->CTRL)&(1<<16))); //定时器检查第16位是否至1  如果为1说明时间到了退出循环
+    }
+     SysTick->CTRL =0x00;
 }
 
 
-void speed_Config(uint8_t* speed)
+void Hall_run(void)
 {
-	speed[7] = 0x00; //左轮方向正转
-	speed[11] = 0x00;//右轮方向正传
-	speed[9] = left&0xFF;
-	speed[8] = left>>8;
-	speed[13] = right&0xFF ;
-	speed[12] = right>>8;
-	cal_crc=ModBus_CRC16_Calculate(speed , sizeof(speed)-2);
-	speed[14]=cal_crc&0xFF;
-	speed[15]=cal_crc>>8; 
-	FillUartTxBufN((uint8_t*)speed, 16, 1);
-}
-
-//速度控制
-void Hall_detect(void)
-{
-		//权重及偏置	
 		int q1=65;
 		int q2=40;
 		int q3=30;
@@ -142,167 +137,34 @@ void Hall_detect(void)
 		int q7=25;
 		int q8=20;	
 		int _const =130;
-		/*
-		* 												0   1   2   3    4   5		6					7						8					9				10 				11 					12 					13				14	 15
- 		* 发送数据格式：01 16 00 38 00 04 Data1H Data1L Data2H Data2L Data3H Data3L Data4H Data4L CRCL CRCH
-		* [Data1H Data1L] 为左轮方向，0-正转，1-倒转
-		* [Data2H Data2L] 为左轮速度，0~600 （当设置为600时，最大速度为0.6m/s）
-		* [Data3H Data3L] 为右轮方向，0-正转，1-倒转
-		* [Data4H Data4L] 为右轮速度，0~600（当设置为600时，最大速度为0.6m/s）
-		*/
-	 if(HallValue[15]+HallValue[14]+HallValue[13]+HallValue[12]+HallValue[11]+HallValue[10]+HallValue[9]+HallValue[8]+HallValue[7]+HallValue[6]+HallValue[5]+HallValue[4]+HallValue[3]+HallValue[2]+HallValue[1]+HallValue[0]+CARD > 0)//有信号
+	 if(HallValue[15]+HallValue[14]+HallValue[13]+HallValue[12]+HallValue[11]+HallValue[10]+HallValue[9]+HallValue[8]+HallValue[7]+HallValue[6]+HallValue[5]+HallValue[4]+HallValue[3]+HallValue[2]+HallValue[1]+HallValue[0]> 0)//有信号
 		 {
 			 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);//蓝灯亮
-			 if(CARD == 0)
-			 {
-				 right =   HallValue[15]*q1+HallValue[14]*q2+HallValue[13]*q3+HallValue[12]*q4+HallValue[11]*q5+HallValue[10]*q6+HallValue[9]*q7+HallValue[8]*q8+_const;
-				 left  =   HallValue[0]*q1+HallValue[1]*q2+HallValue[2]*q3+HallValue[3]*q4+HallValue[4]*q5+HallValue[5]*q6+HallValue[6]*q7+HallValue[7]*q8+_const;
-				 speed_Config(speed);
-			 }
-			 else if(CARD == 1)
-			 {
-					if(COUNT<20)
-					{
-						right= 200;
-						left= 200;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-						COUNT++;
-						speed_Config(speed);
-					}
-					else
-					{
-						CARD=0;
-						COUNT=0;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-					}
-					
-			 }
-			 else if(CARD == 2)
-			 {
-				 if(COUNT <7)
-				{
-					right= 100;
-					left= 10;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-					COUNT++;
-					speed_Config(speed);
-				}
-				else 
-				{
-					CARD = 0;
-					COUNT = 0;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-					}
-			 }			 
-			 else if(CARD == 3)
-			 {				
-				 if(COUNT <7)
-				 {
-					 right= 100;
-					 left= 10;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-					 COUNT++;										
-					 speed_Config(speed);													
-				 }
-				 else 
-				{
-					CARD = 0;
-					COUNT = 0;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-					}
-			 }
-			 else if(CARD == 4)
-			 {
-				 if(COUNT <7)
-				{	
-					right= 10;
-					left= 100;
-					COUNT++;										
-					speed_Config(speed);													
-				}
-				else 
-				{
-					CARD = 0;
-					COUNT = 0;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-					}
-			 }	
-			 else if(CARD == 5)
-			 {
-				 if(COUNT <2)
-				{
-					right= 16;
-					left= 160;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-					COUNT++;										
-					speed_Config(speed);													
-				}
-				else 
-				{
-						CARD = 0;
-						COUNT = 0;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-					}
-			 }	
-			 else if(CARD == 6)
-			 {	
-				 if(COUNT <20)
-					{
-						right= 0;
-						left= 0;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-						COUNT++;										
-						speed_Config(speed);	
-						HAL_Delay(10);
-					}
-					else 
-					{
-						CARD = 0;
-						COUNT = 0;
-						//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-						}
-			 }	
-			 else if(CARD == 7)
-			 {
-				 if(COUNT<5)
-				{
-					right= 200;
-					left= 200;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-					COUNT++;										
-					speed_Config(speed);										
-				}
-				else
-				{
-					CARD=0;
-					COUNT=0;
-					//HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
-				}	
-			 }				 
-			 else if(CARD == 8)
-			 {
-				 right= 0;
-				 left= 0;
-				 COUNT++;										
-				speed_Config(speed);									
-				}
+			 right =   HallValue[15]*q1+HallValue[14]*q2+HallValue[13]*q3+HallValue[12]*q4+HallValue[11]*q5+HallValue[10]*q6+HallValue[9]*q7+HallValue[8]*q8+_const;
+			 left  =   HallValue[0]*q1+HallValue[1]*q2+HallValue[2]*q3+HallValue[3]*q4+HallValue[4]*q5+HallValue[5]*q6+HallValue[6]*q7+HallValue[7]*q8+_const;
+			 speed_Config(left,right);
+		 }
+		 else//冲出导轨
+			{
+				HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);//蓝灯灭
+				FillUartTxBufN((uint8_t*)stop, 16, 1);
 			}
-			 else
-			 {
-				 //HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);//蓝灯灭
-				 speed[7] = 0x00; 
-				 speed[11] = 0x00;
-				 speed[9] = left&0xFF;
-				 speed[8] = left>>8;
-				 speed[13] = right&0xFF ;
-				 speed[12] = right>>8;
-				 cal_crc=ModBus_CRC16_Calculate(speed , sizeof(speed)-2);
-				 speed[14]=cal_crc&0xFF;
-				 speed[15]=cal_crc>>8; 
-				 FillUartTxBufN((uint8_t*)speed, 16, 1);
-			 }  
 }
 
+void speed_Config(uint16_t left, uint16_t right)
+{
+	uint8_t speed1[16] = {0x01,0x16,0x00,0x38,0x00,0x04 ,0x00,0x00,0x00,0x64, 0x00,0x00,0x00,0x64, 0x0B,0x15};//发送数据格式
+	speed1[7] = 0x00; //左轮方向正转
+	speed1[11] = 0x00;//右轮方向正传
+	speed1[9] = left&0xFF;
+	speed1[8] = left>>8;
+	speed1[13] = right&0xFF ;
+	speed1[12] = right>>8;
+	cal_crc=ModBus_CRC16_Calculate(speed1 , sizeof(speed1)-2);
+	speed1[14]=cal_crc&0xFF;
+	speed1[15]=cal_crc>>8; 
+	FillUartTxBufN((uint8_t*)speed1, 16, 1);
+}
 
 //检测三路按键的状态
 void button_detect(void)
@@ -319,14 +181,11 @@ void Infrared_Avoid(void)
 {
 	if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_12)+HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_13)+HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_14)+HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_15)+HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_8)>0)
 	{
-		FillUartTxBufN((uint8_t*)stop, 16, 1);
-		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_SET);//红灯亮
-		
+		STOP=1;
 	}//红外避障
 	else 
 	{
-		//FillUartTxBufN((uint8_t*)go, 16, 1);
-		HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, GPIO_PIN_RESET);//红灯灭
+		STOP=0;
 	}
 }
 
@@ -334,7 +193,7 @@ void Light_Task(void)
 {
 	//GPIO_PIN_RESET（熄灭） & GPIO_PIN_SET（点亮）
 	//黄灯：GPIOE_PIN_3 				蓝灯：GPIOE_PIN_4 				红灯：GPIOE_PIN_5  				绿灯：GPIOE_PIN_6
-	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
 }
 
